9d521563c4603842f94ea5d5b31cca8e0ab8821d
Huy Trinh <h3trinh@uwaterloo.ca>
2025-01-26 22:27:48 -0500

    Test how-to-install on Marmoset for case of remove duplicate

--- A1/src/packages/solvers.rs
+++ A1/src/packages/solvers.rs
@@ -93,118 +93,38 @@ impl Packages {
     /// When a dependency A | B | C is unsatisfied, there are two possible cases:
     ///   (1) there are no versions of A, B, or C installed; pick the alternative with the highest version number (yes, compare apples and oranges).
     ///   (2) at least one of A, B, or C is installed (say A, B), but with the wrong version; of the installed packages (A, B), pick the one with the highest version number.
-    // pub fn compute_how_to_install(&self, package_name: &str) -> Vec<i32> {
-    //     if !self.package_exists(package_name) {
-    //         return vec![];
-    //     }
-
-    //     let mut dependencies_to_add: Vec<i32> = vec![];
-
-    //     let deps = self
-    //         .dependencies
-    //         .get(&self.get_package_num(package_name))
-    //         .unwrap();
-    //     let mut remaining_deps = VecDeque::new();
-    //     remaining_deps.extend(deps.iter());
-
-    //     while let Some(current_dependency) = remaining_deps.pop_front() {
-    //         if self.dep_is_satisfied(current_dependency).is_some() {
-    //             continue;
-    //         }
-
-    //         let wrong_version_packages = self.dep_satisfied_by_wrong_version(current_dependency);
-
-    //         if wrong_version_packages.is_empty() {
-    //             if current_dependency.len() == 1 {
-    //                 let dep_num = current_dependency[0].package_num;
-    //                 if !dependencies_to_add.contains(&dep_num) {
-    //                     dependencies_to_add.push(dep_num);
-    //                     self.dependencies
-    //                         .get(&dep_num)
-    //                         .unwrap()
-    //                         .iter()
-    //                         .for_each(|md| remaining_deps.push_back(md));
-    //                 }
-    //             } else {
-    //                 let best_dep = current_dependency
-    //                     .iter()
-    //                     .map(|el| {
-    //                         let ver = el
-    //                             .rel_version
-    //                             .as_ref()
-    //                             .unwrap()
-    //                             .1
-    //                             .parse::<debversion::DebianVersionNum>()
-    //                             .unwrap();
-    //                         (el, ver)
-    //                     })
-    //                     .max_by(|(_, v1), (_, v2)| v1.cmp(v2))
-    //                     .unwrap();
-
-    //                 if !dependencies_to_add.contains(&best_dep.0.package_num) {
-    //                     dependencies_to_add.push(best_dep.0.package_num);
-    //                     self.dependencies
-    //                         .get(&best_dep.0.package_num)
-    //                         .unwrap()
-    //                         .iter()
-    //                         .for_each(|md| remaining_deps.push_back(md));
-    //                 }
-    //             }
-    //         } else {
-    //             let dep_num = wrong_version_packages
-    //                 .iter()
-    //                 .map(|name| self.get_package_num(name))
-    //                 .max_by(|&a, &b| a.cmp(&b))
-    //                 .unwrap();
-
-    //             if !dependencies_to_add.contains(&dep_num) {
-    //                 dependencies_to_add.push(*dep_num);
-    //                 self.dependencies
-    //                     .get(&dep_num)
-    //                     .unwrap()
-    //                     .iter()
-    //                     .for_each(|md| remaining_deps.push_back(md));
-    //             }
-    //         }
-    //     }
-
-    //     dependencies_to_add
-    // }
     pub fn compute_how_to_install(&self, package_name: &str) -> Vec<i32> {
         if !self.package_exists(package_name) {
             return vec![];
         }
-    
+
         let mut dependencies_to_add: Vec<i32> = vec![];
-    
+
         let deps = self
             .dependencies
             .get(&self.get_package_num(package_name))
             .unwrap();
         let mut remaining_deps = VecDeque::new();
         remaining_deps.extend(deps.iter());
-    
+
         while let Some(current_dependency) = remaining_deps.pop_front() {
-            // Skip dependencies that are already satisfied
             if self.dep_is_satisfied(current_dependency).is_some() {
                 continue;
             }
-    
+
             let wrong_version_packages = self.dep_satisfied_by_wrong_version(current_dependency);
-    
+
             if wrong_version_packages.is_empty() {
                 if current_dependency.len() == 1 {
                     let dep_num = current_dependency[0].package_num;
-    
-                    // Directly add to dependencies_to_add without checking for duplicates
-                    dependencies_to_add.push(dep_num);
-    
-                    // Add all sub-dependencies to the remaining_deps queue
-                    self.dependencies
-                        .get(&dep_num)
-                        .unwrap()
-                        .iter()
-                        .for_each(|md| remaining_deps.push_back(md));
+                    if !dependencies_to_add.contains(&dep_num) {
+                        dependencies_to_add.push(dep_num);
+                        self.dependencies
+                            .get(&dep_num)
+                            .unwrap()
+                            .iter()
+                            .for_each(|md| remaining_deps.push_back(md));
+                    }
                 } else {
                     let best_dep = current_dependency
                         .iter()
@@ -220,38 +140,118 @@ impl Packages {
                         })
                         .max_by(|(_, v1), (_, v2)| v1.cmp(v2))
                         .unwrap();
-    
-                    // Directly add to dependencies_to_add without checking for duplicates
-                    dependencies_to_add.push(best_dep.0.package_num);
-    
-                    // Add all sub-dependencies to the remaining_deps queue
-                    self.dependencies
-                        .get(&best_dep.0.package_num)
-                        .unwrap()
-                        .iter()
-                        .for_each(|md| remaining_deps.push_back(md));
+
+                    if !dependencies_to_add.contains(&best_dep.0.package_num) {
+                        dependencies_to_add.push(best_dep.0.package_num);
+                        self.dependencies
+                            .get(&best_dep.0.package_num)
+                            .unwrap()
+                            .iter()
+                            .for_each(|md| remaining_deps.push_back(md));
+                    }
                 }
             } else {
-                // Choose the wrong version package with the highest version
                 let dep_num = wrong_version_packages
                     .iter()
                     .map(|name| self.get_package_num(name))
                     .max_by(|&a, &b| a.cmp(&b))
                     .unwrap();
-    
-                // Directly add to dependencies_to_add without checking for duplicates
-                dependencies_to_add.push(*dep_num);
-    
-                // Add all sub-dependencies to the remaining_deps queue
-                self.dependencies
-                    .get(&dep_num)
-                    .unwrap()
-                    .iter()
-                    .for_each(|md| remaining_deps.push_back(md));
+
+                if !dependencies_to_add.contains(&dep_num) {
+                    dependencies_to_add.push(*dep_num);
+                    self.dependencies
+                        .get(&dep_num)
+                        .unwrap()
+                        .iter()
+                        .for_each(|md| remaining_deps.push_back(md));
+                }
             }
         }
-    
+
         dependencies_to_add
     }
+    // pub fn compute_how_to_install(&self, package_name: &str) -> Vec<i32> {
+    //     if !self.package_exists(package_name) {
+    //         return vec![];
+    //     }
+    
+    //     let mut dependencies_to_add: Vec<i32> = vec![];
+    
+    //     let deps = self
+    //         .dependencies
+    //         .get(&self.get_package_num(package_name))
+    //         .unwrap();
+    //     let mut remaining_deps = VecDeque::new();
+    //     remaining_deps.extend(deps.iter());
+    
+    //     while let Some(current_dependency) = remaining_deps.pop_front() {
+    //         // Skip dependencies that are already satisfied
+    //         if self.dep_is_satisfied(current_dependency).is_some() {
+    //             continue;
+    //         }
+    
+    //         let wrong_version_packages = self.dep_satisfied_by_wrong_version(current_dependency);
+    
+    //         if wrong_version_packages.is_empty() {
+    //             if current_dependency.len() == 1 {
+    //                 let dep_num = current_dependency[0].package_num;
+    
+    //                 // Directly add to dependencies_to_add without checking for duplicates
+    //                 dependencies_to_add.push(dep_num);
+    
+    //                 // Add all sub-dependencies to the remaining_deps queue
+    //                 self.dependencies
+    //                     .get(&dep_num)
+    //                     .unwrap()
+    //                     .iter()
+    //                     .for_each(|md| remaining_deps.push_back(md));
+    //             } else {
+    //                 let best_dep = current_dependency
+    //                     .iter()
+    //                     .map(|el| {
+    //                         let ver = el
+    //                             .rel_version
+    //                             .as_ref()
+    //                             .unwrap()
+    //                             .1
+    //                             .parse::<debversion::DebianVersionNum>()
+    //                             .unwrap();
+    //                         (el, ver)
+    //                     })
+    //                     .max_by(|(_, v1), (_, v2)| v1.cmp(v2))
+    //                     .unwrap();
+    
+    //                 // Directly add to dependencies_to_add without checking for duplicates
+    //                 dependencies_to_add.push(best_dep.0.package_num);
+    
+    //                 // Add all sub-dependencies to the remaining_deps queue
+    //                 self.dependencies
+    //                     .get(&best_dep.0.package_num)
+    //                     .unwrap()
+    //                     .iter()
+    //                     .for_each(|md| remaining_deps.push_back(md));
+    //             }
+    //         } else {
+    //             // Choose the wrong version package with the highest version
+    //             let dep_num = wrong_version_packages
+    //                 .iter()
+    //                 .map(|name| self.get_package_num(name))
+    //                 .max_by(|&a, &b| a.cmp(&b))
+    //                 .unwrap();
+    
+    //             // Directly add to dependencies_to_add without checking for duplicates
+    //             dependencies_to_add.push(*dep_num);
+    
+    //             // Add all sub-dependencies to the remaining_deps queue
+    //             self.dependencies
+    //                 .get(&dep_num)
+    //                 .unwrap()
+    //                 .iter()
+    //                 .for_each(|md| remaining_deps.push_back(md));
+    //         }
+    //     }
+    
+    //     dependencies_to_add
+    // }
     
 }
